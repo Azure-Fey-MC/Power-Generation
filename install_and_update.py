ee4e3c34e9d9669d8e18f38e4138bd80786c9b31
Teal Wolf 25 <teal@tw25.net>
2026-04-12 05:12:23 -0400

    add error catch

--- install_and_update.py
+++ install_and_update.py
@@ -1,14 +1,19 @@
-import os
+try:
+    import os
 
-if os.path.exists('.git'):
-    os.system('git pull')
-else:
-    os.system('git clone https://github.com/Azure-Fey-MC/Power-Generation.git')
-    repo = os.listdir('Power-Generation')
-    for item in repo:
-        os.rename('Power-Generation/'+item, item)
-    os.rmdir('Power-Generation/')
+    if os.path.exists('.git'):
+        os.system('git pull')
+    else:
+        os.system('git clone https://github.com/Azure-Fey-MC/Power-Generation.git')
+        repo = os.listdir('Power-Generation')
+        for item in repo:
+            os.rename('Power-Generation/'+item, item)
+        os.rmdir('Power-Generation/')
 
-import roll
+    import roll
 
-roll.init()
+    roll.init()
+except Exception as e:
+    print(e)
+    with open("crash.log", "w") as f:
+        f.write(str(e))
